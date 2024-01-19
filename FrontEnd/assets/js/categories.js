77ebe5f52c66c3ec4306b4e1d183ba6cd42596fd
anapeff <anais.pefferkorn@outlook.fr>
2024-01-19 11:46:00 +0100

    Ajout css filtres

--- FrontEnd/assets/js/categories.js
+++ FrontEnd/assets/js/categories.js
@@ -1,39 +1,37 @@
 async function fetchCategories() {
-    try {
-      // Chemin pour l'API categories
-      const response = await fetch("http://localhost:5678/api/categories");
-  
-      // Vérifie si la réponse est OK
-      if (!response.ok) {
-        throw new Error(`Erreur de réseau, statut ${response.status}`);
-      }
-  
-      // Mettre réponse dans un fichier .json
-      const categories = await response.json();
-  
-      console.log('Données récupérées avec succès :', categories);
-      
-      let filtres = document.querySelector('.filtres');
+  try {
+    // Chemin pour l'API categories
+    const response = await fetch("http://localhost:5678/api/categories");
 
-      filtres.innerHTML = '<button class="btn-filtres">Tous</button>';
-
-      for(let i=0; i < categories.length; i++){
-        filtres.innerHTML += '<button class="btn-filtres" data-id="' + categories[i].id + '">' + categories[i].name + '</button>';
-      }
-
-      // Appeler les boutons de filtrages
-      // Créer une boucle "forEach" sur les boutons
-      // Appeler l'id du bouton et le comparer avec celui des travaux
-      // Retourner le résultat
-  
-
-
-
-
-  
-    } catch (error) {
-      console.error('Erreur lors de la récupération des œuvres :', error);
+    // Vérifie si la réponse est OK
+    if (!response.ok) {
+      throw new Error(`Erreur de réseau, statut ${response.status}`);
     }
-  }
 
-  fetchCategories();
+    // Mettre réponse dans un fichier .json
+    const categories = await response.json();
+
+    console.log('Données récupérées avec succès :', categories);
+    
+    let filtres = document.querySelector('.filtres');
+
+    filtres.innerHTML = '<button class="btn-filtres">Tous</button>';
+
+    for(let i=0; i < categories.length; i++){
+      filtres.innerHTML += '<button class="btn-filtres" data-id="' + categories[i].id + '">' + categories[i].name + '</button>';
+    }
+
+    // Appeler les boutons de filtrages
+    // Créer une boucle "forEach" sur les boutons
+    // Appeler l'id du bouton et le comparer avec celui des travaux
+    // Retourner le résultat
+
+
+
+
+  } catch (error) {
+    console.error('Erreur lors de la récupération des œuvres :', error);
+  }
+}
+
+fetchCategories();
