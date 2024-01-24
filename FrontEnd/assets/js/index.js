1266d85992c4dcea86da334ecccadf0ca21d9d3f
anapeff <anais.pefferkorn@outlook.fr>
2024-01-24 09:40:09 +0100

    Réorganisation du code + modification des filtres

---
+++ FrontEnd/assets/js/index.js
@@ -0,0 +1,125 @@
+// Variable globale pour stocker les projets
+let projects;
+
+// Fonction pour créer la galerie avec les projets
+function createGallery(project) {
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+    const titre = document.createElement("figcaption");
+
+    image.src = project.imageUrl;
+    image.alt = project.title;
+    titre.innerHTML = project.title;
+
+    figure.appendChild(image);
+    figure.appendChild(titre);
+
+    return figure;
+}
+
+// Récupérer les projets depuis l'API
+async function fetchWorks() {
+    try {
+        const response = await fetch("http://localhost:5678/api/works");
+
+        if (!response.ok) {
+            throw new Error(`Erreur de réseau, statut ${response.status}`);
+        }
+
+        // Mettre réponse dans un fichier .json
+        projects = await response.json();
+
+        // Appel de la classe "gallery" dans le code html
+        const gallery = document.querySelector(".gallery");
+
+        // Ajouter les projets à la galerie en utilisant la fonction createGallery
+        projects.forEach(project => {
+            gallery.appendChild(createGallery(project));
+        });
+
+    } catch (error) {
+        console.error('Erreur lors de la récupération des œuvres :', error);
+    }
+}
+
+
+fetchWorks();
+
+
+
+
+
+async function fetchCategories() {
+    try {
+        // Chemin pour l'API categories
+        const response = await fetch("http://localhost:5678/api/categories");
+
+       
+        if (!response.ok) {
+            throw new Error(`Erreur de réseau, statut ${response.status}`);
+        }
+
+        // Mettre réponse dans un fichier .json
+        const categories = await response.json();
+
+        console.log('Données récupérées avec succès :', categories);
+
+        let filtres = document.querySelector('.filtres');
+
+        // Créer bouton Tous
+
+        filtres.innerHTML = '<button class="btn-filtres" data-id="all">Tous</button>';
+
+        // Créer autres boutons filtres
+
+        for (let i = 0; i < categories.length; i++) {
+            filtres.innerHTML += '<button class="btn-filtres" data-id="' + categories[i].id + '">' + categories[i].name + '</button>';
+        }
+
+
+
+        const btnFiltres = document.querySelectorAll('.btn-filtres');
+
+
+ // Ajoutez boucles aux boutons filtres
+        btnFiltres.forEach(btn => {
+            btn.addEventListener('click', () => {
+                const categoryId = btn.getAttribute('data-id');
+                filterProjects(categoryId);
+            });
+        });
+
+    } catch (error) {
+        console.error('Erreur lors de la récupération des projets :', error);
+    }
+}
+
+// Fonction pour filtrer les projets en fonction de la catégorie sélectionnée et appliquer le filtre sur la galerie
+async function filterProjects(categoryId) {
+    try {
+        const gallery = document.querySelector(".gallery");
+
+        gallery.innerHTML = ''; // Efface la galerie actuelle
+
+        // Attend la récupération des projets si ce n'est pas déjà fait
+        if (!projects) {
+            await fetchWorks();
+        }
+
+        // Filtrer les projets en fonction de la catégorie
+        const filteredProjects = (categoryId === 'all') ? projects : projects.filter(project => project.categoryId == categoryId);
+
+        // Ajouter les projets à la galerie en utilisant la fonction createGallery
+        filteredProjects.forEach(project => {
+            const figure = createGallery(project);
+            gallery.appendChild(figure);
+        });
+
+        console.log('ok filtres');
+    } catch (error) {
+        console.error('Erreur lors de la filtration et du rendu des projets :', error);
+    }
+}
+
+// Fonction pour récupérer les catégories après l'affichage des projets
+fetchCategories();
